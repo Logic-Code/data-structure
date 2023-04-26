df76982ad902e19573bda43842311f2c0d298228
逻辑可口电影 <logic_code@163.com>
2023-04-26 09:37:48 +0000

    init
    Signed-off-by: 逻辑可口电影 <logic_code@163.com>

---
+++ data-structure/src/main/java/com/astral/ListNode.java
@@ -0,0 +1,11 @@
+package main.java.com.astral;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    //构造函数
+    public ListNode(int a){
+        this.val = a;
+    }
+
+}
---
+++ data-structure/src/test/java/com/astral/Test.java
@@ -0,0 +1,124 @@
+package test.java.com.astral;
+
+
+import main.java.com.astral.ListNode;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.Key;
+import java.util.*;
+
+
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+        //打印链表
+        ListNode headNode1 = new ListNode(1);
+        ListNode listNode1_1 = new ListNode(3);
+        ListNode listNode1_2 = new ListNode(5);
+        ListNode listNode1_3 = new ListNode(7);
+        ListNode listNode1_4 = new ListNode(9);
+
+        ListNode headNode2 = new ListNode(2);
+        ListNode listNode2_1 = new ListNode(4);
+        ListNode listNode2_2 = new ListNode(6);
+        ListNode listNode2_3 = new ListNode(8);
+        ListNode listNode2_4 = new ListNode(10);
+
+        headNode1.next = listNode1_1;
+        listNode1_1.next = listNode1_2;
+        listNode1_2.next = listNode1_3;
+        listNode1_3.next = listNode1_4;
+
+        ListNode cur = headNode1;
+        while(cur != null){
+            System.out.print(cur.val+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+        //反转链表
+        //reverseList(headNode,listNode1,listNode2,listNode3,listNode4);
+        //判断环形链表
+        //listNode4.next = listNode2;
+        //isCircleList(headNode,listNode1,listNode2,listNode3,listNode4);
+        //合并两个有序链表
+        List<ListNode> nodeList1 = new ArrayList<>();
+        nodeList1.add(headNode1);
+        nodeList1.add(listNode1_1);
+        nodeList1.add(listNode1_2);
+        nodeList1.add(listNode1_3);
+        nodeList1.add(listNode1_4);
+        List<ListNode> nodeList2 = new ArrayList<>();
+        nodeList2.add(headNode2);
+        nodeList2.add(listNode2_1);
+        nodeList2.add(listNode2_2);
+        nodeList2.add(listNode2_3);
+        nodeList2.add(listNode2_4);
+        mergeListNode(nodeList1,nodeList2);
+
+    }
+
+    //反转单链表
+    private void reverseList(ListNode headNode,ListNode listNode1,ListNode listNode2,ListNode listNode3, ListNode listNode4){
+        //链表反转
+        ListNode reverseStep = headNode;
+        ListNode prev = null;
+        ListNode reverseTmp = null;
+        while(reverseStep != null){
+            reverseTmp = reverseStep;
+            reverseStep = reverseStep.next;
+            reverseTmp.next = prev;
+            prev = reverseTmp;
+        }
+        ListNode curReverse = listNode4;
+        while(curReverse != null){
+            System.out.print(curReverse.val+" ");
+            curReverse = curReverse.next;
+        }
+        System.out.println();
+    }
+
+    //判断环形链表
+    private void isCircleList(ListNode headNode,ListNode listNode1,ListNode listNode2,ListNode listNode3, ListNode listNode4){
+        Map<Integer,ListNode> map = new HashMap<>();
+        //map.put(headNode.val,headNode);
+        ListNode cur = headNode;
+        while (cur.next != null){
+            if(null != map.get(cur.val)){
+                System.out.println("isCircle:"+cur.val);
+                break;
+            }else{
+                map.put(cur.val,cur);
+            }
+            cur = cur.next;
+        }
+    }
+
+    //两个有序链表合并
+    private void mergeListNode( List<ListNode> nodeList1, List<ListNode> nodeList2){
+        List<ListNode> resultList = new ArrayList<>();
+        ListNode resultNode = null;
+        ListNode curNode1 = null;
+        ListNode curNode2 = null;
+        for(int i = 0;i < nodeList1.size();){
+            curNode1 = nodeList1.get(i);
+            for(int j = 0;j < nodeList2.size();){
+                curNode2 = nodeList2.get(j);
+                if(curNode1.val <= curNode2.val){
+                    resultNode = curNode1;
+                    i++;
+                }else{
+                    resultNode = curNode2;
+                    j++;
+                }
+            }
+        }
+        for(ListNode tmp : resultList){
+            System.out.println(tmp.val);
+        }
+    }
+
+}
