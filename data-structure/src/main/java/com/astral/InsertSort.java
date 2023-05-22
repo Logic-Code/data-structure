06c9e15f37614b9caddbd45c9d6766235169d1c1
haoji <haoji@si-tech.com.cn>
2023-05-22 17:00:42 +0800

    插入排序

--- data-structure/src/main/java/com/astral/InsertSort.java
+++ data-structure/src/main/java/com/astral/InsertSort.java
@@ -10,7 +10,7 @@ public class InsertSort {
         insertSort(oriArray,length);
     }
 
-    //快速排序。将要排序的数组分为两个部分，一个部分是已排序，一个部分未排序
+    //插入排序。将要排序的数组分为两个部分，一个部分是已排序，一个部分未排序
     //每次从未排序中取出一个，循环全部已排序的，将其插入到合适的位置
     private static void insertSort(int oriArray[], int length){
         int resultArray[] = new int[length];
